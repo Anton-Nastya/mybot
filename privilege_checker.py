13c50b12d86c5cfaa90d6b7b374b38778d8267a7
petri <petrichuk.nastya@yandex.ru>
2022-10-04 22:31:20 +0300

    убран чекер привелегий

--- privilege_checker.py
+++ privilege_checker.py
@@ -7,16 +7,7 @@ def privilege_check(bot):
             if message.text == '/exit':
                 return None
 
-            with open("data_files/users.json") as f:
-                users = load(f)
-
-            for user in users:
-                if user['id'] == message.from_user.id:
-                    if (message.text in user['privileges']) or user['admin']:
-                        return func(message)
-
-            bot.send_message(message.from_user.id, "У тебя нет прав для выполнения данной команды")
-            return None
+            return func(message)
 
         return wrapper
 
